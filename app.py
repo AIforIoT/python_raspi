ad690377ff0e3c6dbff849af57bb7a8d71bcb06b
miquelpuigmena <miquelpuigmena@gmail.com>
2018-09-22 22:10:49 +0000

    Bugfix: missing '}' on jsonify functions.

--- app.py
+++ app.py
@@ -6,15 +6,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify('index': "PAE IoT"), 200
+    return jsonify({'index': "PAE IoT"}), 200
 
 @app.route('/impulse', methods=["POST"])
 def treat_impulse():
-    return jsonify({'impulse': request.POST.get('data')), 200
+    return jsonify({'impulse': request.POST.get('data')}), 200
 
 @app.route('/voice', methods=["POST"])
 def treat_voice():
-    return jsonify({'voice': request.POST.get('data')), 200
+    return jsonify({'voice': request.POST.get('data')}), 200
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=911)
