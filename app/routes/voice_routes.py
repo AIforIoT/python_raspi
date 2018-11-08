414fd01be6408ca2612f522f3a30abf032961ff1
celia.santos <celia.santos@atekena.com>
2018-11-08 14:03:48 +0100

    comments

--- app/routes/voice_routes.py
+++ app/routes/voice_routes.py
@@ -4,9 +4,11 @@ import json
 from app.data_service.data_service import Data_service
 from app.models.data_request_object import FrameData, ConfigParams
 
-bp = Blueprint('voice_routes', __name__, url_prefix='/espData')
+bp = Blueprint('voice_routes', __name__)
 
 data_service = Data_service()
+
+timeout = None
 
 @bp.route('/impulse', methods=['POST'])
 def get_impulse():
@@ -26,3 +28,16 @@ def save_esp_setup_data():
     data_service.save_esp_setup_data(request.data)
 
     return 'OK', 200
+
+
+@bp.route('/volume', methods=['POST'])
+def get_volume():
+
+    #TODO:
+    #1: Store volume data in memory.
+    #2: timeout = Timer(1,fn,args)
+    return None
+
+def timer():
+    ...
+    timeout = None
