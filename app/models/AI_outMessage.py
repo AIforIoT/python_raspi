9c5e84b9691f57ad4761aee6f8abcfc9e3de60e0
JMoya1097 <40645441+JMoya1097@users.noreply.github.com>
2018-11-19 10:59:58 +0100

    Add files via upload

---
+++ app/models/AI_outMessage.py
@@ -0,0 +1,21 @@
+
+class outputMessage:
+
+    def init(self,iouti,status,location,typeObj):
+        self.iouti = iouti
+        self.status = status
+        self.location = location
+        self.typeObj = typeObj
+ 
+    def getIouti(self):
+        return self.iouti
+    
+    def getStatus(self):
+        return self.status
+
+    def getLocation(self):
+        return self.location
+
+    def getTypeObj(self):
+        return self.typeObj
+                         
