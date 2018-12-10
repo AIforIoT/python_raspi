58a9daecf927e68dfc55468125ab592a2e42ab29
celia.santos <celia.santos@atekena.com>
2018-12-10 11:45:11 +0100

    init

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,14 @@
 from flask import Flask
 from app.database.database import db_session, init_db
-from app.routes import voice_routes, data_controller, test_db_endpoints
+from app.routes import voice_routes, data_controller
 from app.data_service import data_service
 from app.models.data_request_object import FrameData
 
-from flask import Flask
-from app.database.database import db_session, init_db
+app = Flask(__name__)
+app.register_blueprint(voice_routes.bp)
+app.register_blueprint(data_controller.bp)
 
-def create_app():
-    # create and configure the app
-    app = Flask(__name__)
+init_db()
 
-    from .routes import voice_routes, data_controller
-    app.register_blueprint(voice_routes.bp)
-    app.register_blueprint(data_controller.bp)
-    app.register_blueprint(test_db_endpoints.bp)
-
-    from .data_service import data_service
-    from .models.data_request_object import FrameData
-
-    init_db()
-
-    return app
-
-
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080, debug=True)
