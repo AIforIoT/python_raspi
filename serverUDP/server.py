0f8a4c278fa6b608568856266264751641d7e4a8
Biel Tura <bieltv.3@gmail.com>
2019-01-22 17:51:25 +0100

    Test for editing files

--- serverUDP/server.py
+++ serverUDP/server.py
@@ -6,10 +6,7 @@ server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 # Set a timeout so the socket does not block
 # indefinitely when trying to receive data.
 
-
 server.settimeout(0.2)
-
-
 
 server.bind(("", 33333))
 message = b"sync"
